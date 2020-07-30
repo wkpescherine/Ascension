e7d1e7d36e24f98c8bfbe0163ff78db625a52569
wkpescherine <wkpescherine@yahoo.com>
2020-07-29 23:20:48 -0400

    Add files via upload

--- Ascension/ASCENSION.java
+++ Ascension/ASCENSION.java
@@ -6,15 +6,19 @@ import javax.swing.*;
 import java.lang.String;
 
 public class ASCENSION implements ActionListener{
-    String [] attributes = {"Race", "Style", "Class", "Name"};
-    int [][] statsTemp = new int[3][10];
-    int [] stats = new int[10];
+    String [] attributes = {"Style", "Race", "Class", "Name"};
+    int [][] statsTemp = new int[3][6];
+    int [] stats = new int[8];
+    int [] statsBonus = new int[8];
+
     String strength = "Strength      : ";
     String stamina = "Stamina        : ";
     String dexterity = "Dexterity     : ";
     String quickness = "Quickmess      : ";
     String intelligence = "Intelligence   : ";
     String spirit = "Spirit         : ";
+    String life = "Life       :";
+    String power = "Power    : ";
 
     JFrame window = new JFrame("Ascension v1");
     JPanel main = new JPanel();
@@ -32,21 +36,23 @@ public class ASCENSION implements ActionListener{
     JButton backToMain2 = new JButton("Back to Main");
     JButton start = new JButton("Start Game");
     JButton human = new JButton("Human");
-    JButton dwarf = new JButton("Dwarf");
+    JButton dwarf = new JButton("Dwarven");
     JButton elven = new JButton("Elven");
     JButton divine = new JButton("Divine");
-    JButton diabolic = new JButton("Diabolic");
+    JButton diabolic = new JButton("Diabolical");
     JButton wise = new JButton("Wise");
     JButton warrior = new JButton("Warrior");
     JButton rogue = new JButton("Rogue");
     JButton mage = new JButton("Mage");
-    JLabel raceStyleClass = new JLabel(attributes[0]+attributes[1]+attributes[2]);
+    JLabel styleRaceClass = new JLabel(attributes[0]+attributes[1]+attributes[2]);
     JLabel str = new JLabel(strength);
     JLabel dex = new JLabel(dexterity);
     JLabel qui = new JLabel(quickness);
     JLabel iq = new JLabel(intelligence);
     JLabel sta = new JLabel(stamina);
     JLabel spr = new JLabel(spirit);
+    JLabel hp = new JLabel(life);
+    JLabel pow = new JLabel(power);
 
     int IFW = JComponent.WHEN_IN_FOCUSED_WINDOW;
 
@@ -73,37 +79,12 @@ public class ASCENSION implements ActionListener{
 
         newGame.setLayout(new FlowLayout());
         newGame.setPreferredSize(new Dimension(780,580));
-        selectRace.setPreferredSize(new Dimension(250,170));
-        selectRace.add(human);
-        human.addActionListener(
-			new ActionListener(){
-				public void actionPerformed(ActionEvent e){
-                    setStats("Human",0,2,2,2,2,2,2);
-				}
-			}
-        );
-        selectRace.add(dwarf);
-        dwarf.addActionListener(
-			new ActionListener(){
-				public void actionPerformed(ActionEvent e){
-                    setStats("Dwarf",0,1,3,2,2,2,2);
-				}
-			}
-        );
-        selectRace.add(elven);
-        elven.addActionListener(
-			new ActionListener(){
-				public void actionPerformed(ActionEvent e){
-                    setStats("Elven",0,1,3,2,2,2,2);
-				}
-			}
-        );
         selectStyle.setPreferredSize(new Dimension(250,170));
         selectStyle.add(divine);
         divine.addActionListener(
 			new ActionListener(){
 				public void actionPerformed(ActionEvent e){
-                    setStats("Divine",1,2,2,2,2,2,2);
+                    setStats("Divine",0,1,1,1,2,3,4);
 				}
 			}
         );
@@ -111,7 +92,7 @@ public class ASCENSION implements ActionListener{
         diabolic.addActionListener(
 			new ActionListener(){
 				public void actionPerformed(ActionEvent e){
-                    setStats("Diabolic",1,1,3,2,2,2,2);
+                    setStats("Diabolic",0,5,3,1,2,1,0);
 				}
 			}
         );
@@ -119,7 +100,32 @@ public class ASCENSION implements ActionListener{
         wise.addActionListener(
 			new ActionListener(){
 				public void actionPerformed(ActionEvent e){
-                    setStats("Wise",1,1,3,2,2,2,2);
+                    setStats("Wise",0,1,1,3,2,5,1);
+				}
+			}
+        );
+        selectRace.setPreferredSize(new Dimension(250,170));
+        selectRace.add(human);
+        human.addActionListener(
+			new ActionListener(){
+				public void actionPerformed(ActionEvent e){
+                    setStats("Human",1,2,2,2,2,2,2);
+				}
+			}
+        );
+        selectRace.add(dwarf);
+        dwarf.addActionListener(
+			new ActionListener(){
+				public void actionPerformed(ActionEvent e){
+                    setStats("Dwarf",1,2,4,1,1,1,1);
+				}
+			}
+        );
+        selectRace.add(elven);
+        elven.addActionListener(
+			new ActionListener(){
+				public void actionPerformed(ActionEvent e){
+                    setStats("Elven",1,1,1,2,2,4,2);
 				}
 			}
         );
@@ -128,7 +134,7 @@ public class ASCENSION implements ActionListener{
         warrior.addActionListener(
 			new ActionListener(){
 				public void actionPerformed(ActionEvent e){
-                    setStats("Warrior",2,2,2,2,2,2,2);
+                    setStats("Warrior",2,4,4,3,1,0,0);
 				}
 			}
         );
@@ -136,7 +142,7 @@ public class ASCENSION implements ActionListener{
         rogue.addActionListener(
 			new ActionListener(){
 				public void actionPerformed(ActionEvent e){
-                    setStats("Rogue",2,2,2,2,2,2,2);
+                    setStats("Rogue",2,2,1,4,5,0,0);
 				}
 			}
         );
@@ -144,7 +150,7 @@ public class ASCENSION implements ActionListener{
         mage.addActionListener(
 			new ActionListener(){
 				public void actionPerformed(ActionEvent e){
-                    setStats("Mage",2,2,2,2,2,2,2);
+                    setStats("Mage",2,0,0,1,3,5,3);
 				}
 			}
         );
@@ -168,17 +174,19 @@ public class ASCENSION implements ActionListener{
         ); 
         bonusSection.setPreferredSize(new Dimension(250,350));
         bonusSection.setLayout(new FlowLayout());
-        bonusSection.add(raceStyleClass);
+        bonusSection.add(styleRaceClass);
         bonusSection.add(str);
         bonusSection.add(sta);
         bonusSection.add(dex);
         bonusSection.add(qui);
         bonusSection.add(iq);
         bonusSection.add(spr);
+        bonusSection.add(hp);
+        bonusSection.add(pow);
         skillsSection.setPreferredSize(new Dimension(500,350));        
         newGame.setBackground(Color.BLACK);
-        newGame.add(selectRace);
         newGame.add(selectStyle);
+        newGame.add(selectRace);
         newGame.add(selectClass);
         newGame.add(bonusSection);
         newGame.add(skillsSection);
@@ -222,7 +230,9 @@ public class ASCENSION implements ActionListener{
     }
     public void actionPerformed(ActionEvent e){
 		main.setVisible(false);
-		newGame.setVisible(true);
+        newGame.setVisible(true);
+        setStats("Name",0,0,0,0,0,0,0);
+        styleRaceClass.setText("");
     }
     
     public void setStats(String choice, int a, int b, int c, int d, int e, int f , int g){
@@ -233,16 +243,20 @@ public class ASCENSION implements ActionListener{
         statsTemp [a][3] = e;
         statsTemp [a][4] = f;
         statsTemp [a][5] = g;
-        stats [0] = statsTemp [0][0] + statsTemp [1][0]+statsTemp [1][0];
-        stats [1] = statsTemp [0][1] + statsTemp [1][1]+statsTemp [1][1];
-        stats [2] = statsTemp [0][2] + statsTemp [1][2]+statsTemp [1][2];
-        stats [3] = statsTemp [0][3] + statsTemp [1][3]+statsTemp [1][3];
-        stats [4] = statsTemp [0][4] + statsTemp [1][4]+statsTemp [1][4];
-        stats [5] = statsTemp [0][5] + statsTemp [1][5]+statsTemp [1][5];
-        raceStyleClass.setText(attributes[0]+" "+attributes[1]+" "+attributes[2]);
-        str.setText(strength+ stats[0]);
-
-        System.out.println(attributes[0]+" "+attributes[1]+" "+attributes[2] + " "+ statsTemp[0][0]);
+        stats [0] = statsTemp [0][0] + statsTemp [1][0]+statsTemp [2][0];
+        stats [1] = statsTemp [0][1] + statsTemp [1][1]+statsTemp [2][1];
+        stats [2] = statsTemp [0][2] + statsTemp [1][2]+statsTemp [2][2];
+        stats [3] = statsTemp [0][3] + statsTemp [1][3]+statsTemp [2][3];
+        stats [4] = statsTemp [0][4] + statsTemp [1][4]+statsTemp [2][4];
+        stats [5] = statsTemp [0][5] + statsTemp [1][5]+statsTemp [2][5];
+        calculateBonuses();
+        styleRaceClass.setText(attributes[0]+"     "+attributes[1]+"      "+attributes[2]);
+        str.setText(strength+ stats[0]+"     +"+statsBonus[0] +"   ");
+        sta.setText(stamina+ stats[1]+"     +"+statsBonus[1] +"   ");
+        dex.setText(dexterity+ stats[2]+"     +"+statsBonus[2] +"   ");
+        qui.setText(quickness+ stats[3]+"     +"+statsBonus[3] +"   ");
+        iq.setText(intelligence+ stats[4]+"     +"+statsBonus[4] +"   ");
+        spr.setText(spirit+ stats[5]+"     +"+statsBonus[5] +"              ");
     }
 
     public void movement(){
@@ -276,5 +290,19 @@ public class ASCENSION implements ActionListener{
 				movement();
 			}
 		});	
-	}
+    }
+    
+    public void calculateBonuses(){
+        for(int i = 0; i<6; i++){
+            int bonusTemp = 0;
+            int modBonus = 0;
+            for(int x = 0; x<stats[i]; x++){
+                bonusTemp += x;
+                if(bonusTemp+x < stats[i]){
+                    modBonus += 1;
+                }
+            }
+            statsBonus[i] = modBonus;
+        }
+    }
 }
