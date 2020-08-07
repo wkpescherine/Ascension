f11b70141df7eb17be356a4fe7257905ad70f2b7
wkpescherine <wkpescherine@yahoo.com>
2020-08-06 22:21:23 -0400

    Add files via upload

---
+++ LordsOfBellum/main/java/com/example/lordsofbellum/SelectLord.java
@@ -0,0 +1,14 @@
+package com.example.lordsofbellum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectLord extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_lord);
+    }
+}
