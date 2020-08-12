80738cd1b1a7e708573353d053e233bff8a3e577
wkpescherine <wkpescherine@yahoo.com>
2020-08-11 22:49:47 -0400

    Add files via upload

--- LordsOfBellum/Dungeon.java
+++ LordsOfBellum/Dungeon.java
@@ -3,13 +3,15 @@ package com.example.lordsofbellum;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
 public class Dungeon extends AppCompatActivity {
 
-    int xCoord = 150;
+    int xCoord = 125;
+    int yCoord = 225;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +20,23 @@ public class Dungeon extends AppCompatActivity {
         setSquare();
     }
 
+    public void moveDown(View v){
+        xCoord -= 50;
+        setSquare();
+    }
+
     public void moveUp(View v){
         xCoord += 50;
         setSquare();
     }
 
-    public void moveDown(View v){
-        xCoord -= 50;
+    public void moveLeft(View v){
+        yCoord -= 50;
+        setSquare();
+    }
+
+    public void moveRight(View v){
+        yCoord += 50;
         setSquare();
     }
 
@@ -32,7 +44,12 @@ public class Dungeon extends AppCompatActivity {
         LinearLayout ll = findViewById(R.id.moveThis);
         ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams)
                 ll.getLayoutParams();
-        params.setMargins(150,xCoord,0,0);
+        params.setMargins(yCoord,xCoord,0,0);
         ll.setLayoutParams(params);
+    }
+
+    public void returnHome(View v){
+        Intent intent = new Intent(this, HomeArea.class);
+        startActivity(intent);
     }
 }
