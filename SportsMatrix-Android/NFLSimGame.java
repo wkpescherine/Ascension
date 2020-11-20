784c75c04ac68f1f3043aff5ee483d440040eaaa
wkpescherine <wkpescherine@yahoo.com>
2020-11-19 22:59:48 -0500

    Add files via upload

--- SportsMatrix-Android/NFLSimGame.java
+++ SportsMatrix-Android/NFLSimGame.java
@@ -2,8 +2,13 @@ package com.example.sportsmatrix;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.text.Layout;
+import android.widget.Button;
+import android.view.View;
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
 
 public class NFLSimGame extends AppCompatActivity {
 
@@ -16,4 +21,19 @@ public class NFLSimGame extends AppCompatActivity {
         getSupportActionBar().hide();
     }
 
-}//19
+    public void pickQB(View v){
+        setContentView(R.layout.nfl_pick_player);
+    }
+
+    public void startGame(View v){
+        Button homeBTN = findViewById(R.id.homeBTN);
+        LinearLayout roster = findViewById(R.id.playerChoices);
+        roster.setVisibility(View.INVISIBLE);
+        homeBTN.setVisibility(View.INVISIBLE);
+    }
+
+    public void Home(View v){
+        Intent intent = new Intent(this, SportChoice.class);
+        startActivity(intent);
+    }
+}//39
