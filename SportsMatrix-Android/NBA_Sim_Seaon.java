0562999f29f1818df9c5352baa6691b34bc6d567
wkpescherine <wkpescherine@yahoo.com>
2020-11-20 23:55:48 -0500

    Add files via upload

--- SportsMatrix-Android/NBA_Sim_Seaon.java
+++ SportsMatrix-Android/NBA_Sim_Seaon.java
@@ -2,7 +2,9 @@ package com.example.sportsmatrix;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
 public class NBA_Sim_Seaon extends AppCompatActivity {
@@ -14,5 +16,14 @@ public class NBA_Sim_Seaon extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
+    }
+
+    public void Back(View v){
+        Intent intent = new Intent(this, SportChoice.class);
+        startActivity(intent);
+    }
+
+    public void pick_Knicks(View v){
+        setContentView(R.layout.nba_sim_season_roster);
     }
 }
