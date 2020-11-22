8fc3a9b479cd80e9696638694611cfeb3c4d482c
wkpescherine <wkpescherine@yahoo.com>
2020-11-22 01:27:01 -0500

    Add files via upload

--- SportsMatrix-Android/MainActivity.java
+++ SportsMatrix-Android/MainActivity.java
@@ -6,8 +6,13 @@ import android.content.Intent;
 import android.view.View;
 import android.view.WindowManager;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    String username = "TestMode";
+    String password = "test";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +29,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void logIn(View v){
-
+        setContentView(R.layout.main_log_in);
     }
 
     public void createAcct(View v){
-
+        setContentView(R.layout.main_create_acct);
     }
-}//33
+
+    public void checkToLogIn(View v){
+        EditText getUsername = findViewById(R.id.logUserName);
+        String UserNameInput = getUsername.getText().toString();
+        EditText getPassword = findViewById(R.id.logPasword);
+        String PasswordInput = getPassword.getText().toString();
+        if(username != UserNameInput || password!= PasswordInput){
+            Toast.makeText(MainActivity.this, "Username or Passowrd incorrect.", Toast.LENGTH_SHORT).show();
+        }else{
+            Intent intent = new Intent(this, SportChoice.class);
+            startActivity(intent);
+        }
+    }
+
+    public void backToMain(View v){
+        setContentView(R.layout.activity_main);
+    }
+}//55
