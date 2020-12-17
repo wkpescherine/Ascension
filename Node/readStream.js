987b576aa56b9d7cfd6fde9964f36a036adcb956
wkpescherine <wkpescherine@yahoo.com>
2020-12-16 23:55:47 -0500

    Delete readStream.js

--- Node/readStream.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-var rs = fs.createReadStream('./newFile3.txt');
-var events = require('events');
-var eventEmitter = new events.EventEmitter();
-
-rs.on('open', function(){
-    console.log('This file is now open');
-})
