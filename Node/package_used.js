39398b1b653887f49265dc65387fb77cc63e72ab
wkpescherine <wkpescherine@yahoo.com>
2020-12-16 23:55:27 -0500

    Delete package_used.js

--- Node/package_used.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-var uc = require('upper-case');
-
-http.createServer(function (req,res){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(uc.upperCase('Hello This wIlL Show AS aLl UPPer case'));
-    res.end();
-}).listen(8080);
